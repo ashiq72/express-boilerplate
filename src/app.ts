8cee20471bb2aa058864d4817e19a05473ed02e4
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-11-23 07:10:48 +0600

    updated

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+// import globalErrorHandler from "./app/middlewares/globalErrorHandeller";
+import router from "./allRoutes";
+// import cookieParser from "cookie-parser";
+const app = express();
+
+// parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+app.use(cors());
+
+//applications routes
+app.use("/api/v1", router);
+
+app.get("/", async (req: Request, res: Response) => {
+  var a = 10;
+  res.send(a.toString());
+});
+
+// app.use(globalErrorHandler);
+
+export default app;
